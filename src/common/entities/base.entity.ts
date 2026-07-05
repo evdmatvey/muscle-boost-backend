5ed92bc8705860ae4783a0101b0dcab286cb3f7e
evdmatvey <evd.matvey@mail.ru>
2026-07-05 16:59:49 +0700

    Add base database components

---
+++ src/common/entities/base.entity.ts
@@ -0,0 +1,20 @@
+import {
+  CreateDateColumn,
+  DeleteDateColumn,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+export abstract class BaseEntity {
+  @PrimaryGeneratedColumn('uuid')
+  public id!: string;
+
+  @CreateDateColumn({ type: 'timestamptz' })
+  public createdAt!: Date;
+
+  @UpdateDateColumn({ type: 'timestamptz' })
+  public updatedAt!: Date;
+
+  @DeleteDateColumn({ type: 'timestamptz' })
+  public deletedAt!: Date | null;
+}
