77842baba452a917195dfc5fe18aa053cbed3060
evdmatvey <evd.matvey@mail.ru>
2026-07-18 22:16:09 +0700

    fix(auth): parse mobile device name from client user-agent

--- src/modules/auth/utils/parse-device-name.util.ts
+++ src/modules/auth/utils/parse-device-name.util.ts
@@ -2,10 +2,16 @@ import { UAParser } from 'ua-parser-js';
 
 export const parseDeviceName = (userAgent: string): string => {
   const parser = new UAParser(userAgent);
-  const browser = parser.getBrowser();
+  const device = parser.getDevice();
   const os = parser.getOS();
-  const browserName = browser.name ?? 'Unknown browser';
   const osName = os.name ?? 'Unknown OS';
 
+  if (device.model) {
+    const osLabel = os.version ? `${osName} ${os.version}` : osName;
+    return `${device.model} / ${osLabel}`;
+  }
+
+  const browser = parser.getBrowser();
+  const browserName = browser.name ?? 'Unknown browser';
   return `${browserName} / ${osName}`;
 };
